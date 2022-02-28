85c9c105552e42f8f3d555f8ea52c9e8dc8a8f52
yunsuuu <dbstnqls6953@naver.com>
2022-02-28 19:51:22 +0900

    .

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
+// 정답 단어 랜덤 배정
+// input칸 하나에 알파벳 하나
+// input 커서 자동 이동
+
 const $form = document.querySelector("form");
 const $input = document.querySelectorAll(".input");
 const $button = document.querySelector("button");
-const answer = "apple";
+const array = ["clock", "enjoy", "fresh", "photo", "smart"];
+const answer = array[Math.floor(Math.random() * array.length)];
+
+console.log(answer);
+
+function moveFocus() {
+  for(let i = 0; i < 4; i++){
+    $input[i].addEventListener("keyup", function(e){
+      if($input[i].value.length === 1){
+        $input[i + 1].focus();
+        return;
+      };
+    })
+  }
+}
+moveFocus();
+
+function makeNew() {
+  const template = `
+  <form class="form">
+    <input class="input" type="text" maxlength="1" required>
+    <input class="input" type="text" maxlength="1" required>
+    <input class="input" type="text" maxlength="1" required>
+    <input class="input" type="text" maxlength="1" required>
+    <input class="input" type="text" maxlength="1" required>
+    <button>제출</button>
+  </form>`;
+  document.querySelector("body").insertAdjacentHTML("beforeend", template);
+}
 
 function onSubmit(e) {
   e.preventDefault();
-  for(let i = 0; i < 5; i++){
+  for(let i = 0; i < $input.length; i++){
     const value = e.target.children[i].value;
     if(value === answer[i]){
       $input[i].style.backgroundColor = "lightgreen";
@@ -15,14 +47,6 @@ function onSubmit(e) {
       $input[i].style.backgroundColor = "lightgray";
     }
   }
-  const template = 
-  `<div>
-    <input class="input">
-    <input class="input">
-    <input class="input">
-    <input class="input">
-    <input class="input">
-  </div>`;
-  document.querySelector("body").insertAdjacentHTML(behin)
+  makeNew();
 }
 $form.addEventListener("submit", onSubmit);
